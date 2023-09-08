b33c4364d1dc28eae465cd0d09dcaf122b387e6f
rahmat rafii <rahmatrafiindrayani@gmail.com>
2023-09-08 18:19:44 +0700

    memperbaiki categories channel detail

--- src/components/ChannelCategories.tsx
+++ src/components/ChannelCategories.tsx
@@ -1,4 +1,7 @@
-import React, { useState } from "react";
+"use client";
+import { motion } from "framer-motion";
+
+import React, { useEffect, useState, useRef } from "react";
 const button = [
   "BERANDA",
   "VIDEO",
@@ -10,29 +13,36 @@ const button = [
   "PENELUSURAN",
 ];
 const ChannelCategories = () => {
+  const [width, setWidth] = useState(0);
+  const carousel = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const scrollWidth = carousel.current ? carousel.current.scrollWidth : 0;
+    const offsetWidth = carousel.current ? carousel.current.offsetWidth : 0;
+    setWidth(scrollWidth - offsetWidth);
+  }, []);
   return (
     <div className="my-5">
       <nav className="shadow-lg relative md:shadow-none md:border-b">
-        {/* <button className="absolute left-0 bottom-0 p-3 rounded-full bg-white">
-          <div className="w-4 h-4 border-l-[1px] border-b-[1px] border-black rotate-45" />
-        </button> */}
-        <div className={` flex transition`}>
-          {button.map((item) => (
-            <button
-              key={item}
-              className={`${
-                item === "VIDEO"
-                  ? "border-b-[1px] border-black md:border-b-[2px]"
-                  : ""
-              } flex-shrink-0  w-[130px] font-medium text-sm flex-center py-3 mr-1 `}
-            >
-              {item}
-            </button>
-          ))}
-        </div>
-        {/* <button className="absolute right-0 p-3 rounded-full bottom-0 bg-white">
-          <div className="w-4 h-4 border-l-[1px] border-b-[1px] border-black -rotate-[135deg]" />
-        </button> */}
+        <motion.div ref={carousel} className="mx-2  overflow-hidden">
+          <motion.div
+            drag="x"
+            dragConstraints={{ right: 0, left: -width }}
+            className="flex w-max px-4  "
+          >
+            {button.map((data: string) => (
+              <motion.button
+                key={data}
+                className={`${
+                  data === "VIDEO"
+                    ? "border-b-[1px] border-black md:border-b-[2px]"
+                    : ""
+                } flex-shrink-0  w-[130px] font-medium text-sm flex-center py-3 mr-1 `}
+              >
+                <span>{data}</span>
+              </motion.button>
+            ))}
+          </motion.div>
+        </motion.div>
       </nav>
     </div>
   );
